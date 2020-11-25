db9f631fc91b8c820eecd9aebe9362d4c9eabfa8
tankistqazwsx <alexei.bezruchenko@yandex.ru>
2020-11-25 12:47:23 +0300

    Added assignees, agents and applicants excraction

--- parser.py
+++ parser.py
@@ -1,19 +1,9 @@
 import re
 import os
+import sys
+from pprint import pprint
 from bs4 import BeautifulSoup as BS
 import time
-
-def get_input_names():
-    os.chdir('input')
-    input_list = list(filter(lambda x: re.match('.*.xml', x), os.listdir()))
-    os.chdir('..')
-    return input_list
-
-def get_patents(path: str):
-    input_file = open(path, 'r')
-    input_string = input_file.read()
-    input_file.close()
-    return input_string
 
 def parse(text: str):
     splitter = '<?xml version="1.0" encoding="UTF-8"?>'
@@ -21,61 +11,64 @@ def parse(text: str):
     splitted = list(filter(lambda x: x != '', splitted))
     return splitted
 
-names = get_input_names()
+def main():
+    names = sys.argv[1:]
+    overall_res = []
+    time_start = time.clock()
+    for filename in names:
+        print("processing " + filename)
+        with open(filename, "r") as f:
+            text = f.read()
+        res = parse(text)
+        for text_num, text in enumerate(res):
+            print(f"text #{text_num}")
+            soup = BS(text)
+            try:
+                # Identification of a patent application.
+                temp = soup.find("application-reference")
+                if temp:
+                    application_reference = {'doc_number': temp.find("doc-number").text,
+                                             'country': temp.find("country").text,
+                                             'date': temp.find("date").text}
+                else:
+                    application_reference = None
+                # Identification of a published patent document.
+                temp = soup.find("publication-reference")
+                if temp:
+                    publication_reference = {'doc-number': temp.find("doc-number").text,
+                                             'country': temp.find("country").text,
+                                             'date': temp.find("date").text}
+                else:
+                    publication_reference = None
+                description = soup.find("description").text
+                claims = [x.text for x in soup.findAll("claim-text")]
 
-names = ["ipg100105.xml"]
-overall_res = []
-time_start = time.clock()
-for filename in names:
-    print("processing " + filename)
-    with open(filename, "r") as f:
-        text = f.read()
-    res = parse(text)
-    for text_num, text in enumerate(res):
-        print(f"text #{text_num}")
-        soup = BS(text)
-        try:
-            # Identification of a patent application.
-            temp = soup.find("application-reference")
-            if temp:
-                application_reference = {'doc_number': temp.find("doc-number").text,
-                                         'country': temp.find("country").text,
-                                         'date': temp.find("date").text}
-            else:
-                application_reference = None
-            # Identification of a published patent document.
-            temp = soup.find("publication-reference")
-            if temp:
-                publication_reference = {'doc_number': temp.find("doc-number").text,
-                                         'country': temp.find("country").text,
-                                         'date': temp.find("date").text}
-            else:
-                publication_reference = None
-            description = soup.find("description").text
-            claims = [x.text for x in soup.findAll("claim-text")]
-            overall_res.append({
-                "application_reference": application_reference,
-                "publication_reference": publication_reference,
-                "description" : description,
-                "claims": claims})
-        except AttributeError:
-            pass
+                applicants = []
+                for applicant in soup.findAll("applicant"):
+                    applicants.append({
+                        "first-name":applicant.find("first-name").text,
+                        "last-name":applicant.find("last-name").text})
 
+                agents = []
+                for agent in soup.find("parties").findAll("agent"):
+                    agents.append({"orgname":agent.find("orgname").text})
 
+                assignees = []
+                for assignee in soup.findAll("assignee"):
+                    assignees.append({"orgname": assignee.find("orgname").text})
 
+                overall_res.append({
+                    "application_reference": application_reference,
+                    "publication_reference": publication_reference,
+                    "description" : description,
+                    "claims": claims,
+                    "applicants": applicants,
+                    "agents":agents,
+                    "assignees":assignees})
+            except AttributeError:
+                pass
+    pprint(overall_res)
+    input()
 
-        # parties
-        # applicant
-        # agent
-        # assignees
-        # examiners
-
-
-
-
-
-    # input_path = 'input/' + filename
-    # patents = get_patents(input_path)
-    # parsed = parse(patents)
-    # save_parsed(parsed, 'output/' + filename)
-    # print(filename +" processed")
+if __name__=="__main__":
+    main()
